c6440040e6d1d774b29c4ea8598ef6c9ba6e6f44
Yehudah <jros987@gmail.com>
2020-12-14 16:27:28 -0500

    joins table

--- reddit/config/routes.rb
+++ reddit/config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
  
   resources :users, only: [:new, :create, :show, :index] 
   resource :session, only: [:create, :new, :destroy]
-  resources :subs, except: [:destroy] do 
-    resources :posts, only: [:index]
-  end
-  resources :posts, except: [:index]
+  resources :subs, except: [:destroy]
+   resources :posts
 end
--- reddit/db/schema.rb
+++ reddit/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_202000) do
+ActiveRecord::Schema.define(version: 2020_12_14_212435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_12_14_202000) do
     t.string "content"
     t.integer "sub_id", null: false
     t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "postsubs", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "sub_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
